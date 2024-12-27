1454ea2d8b431db3e274d9357eb157a37ef0a210
BesarAjruli <aquila1912@gmail.com>
2024-12-27 21:04:16 +0100

    Fixed binding issues

--- app.js
+++ app.js
@@ -9,5 +9,5 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", usersRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Express app listening on port ${PORT}!`));
