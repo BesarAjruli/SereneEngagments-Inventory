b20f003a94668cd3ce9c7fbd52fa46b39fcd14af
BesarAjruli <aquila1912@gmail.com>
2024-12-27 23:29:04 +0100

    Envoirment variables added

--- db/pool.js
+++ db/pool.js
@@ -3,6 +3,10 @@ const {Pool} = require('pg')
 
 
 module.exports = new Pool({
-    connectionString: 'postgres://koyeb-adm:ksD4J3VRdwpC@ep-empty-hat-a22x2nts.eu-central-1.pg.koyeb.app/koyebdb',
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DATABASE_PORT,
     ssl: {rejectUnauthorized: false}
 })
