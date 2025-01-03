69be4600f4c3fd7850ac00dea50e397232bd5ca7
BesarAjruli <aquila1912@gmail.com>
2025-01-03 02:34:09 +0100

    fixed get not awaited and id missing on update

--- db/querry.js
+++ db/querry.js
@@ -8,11 +8,11 @@ return rows
 async function createItem(req) {
     const {buffer} = req.file
     const {name, quantity, price} = req.body
-    pool.query('INSERT INTO inventory (name, quantity, price, image) VALUES ($1, $2, $3, $4)', [name, quantity, price, buffer])
+    await pool.query('INSERT INTO inventory (name, quantity, price, image) VALUES ($1, $2, $3, $4)', [name, quantity, price, buffer])
 }
 
 async function searchItem(search){
-    const {rows} = await pool.query("SELECT * FROM inventory WHERE LOWER(name) LIKE '%' || LOWER($1) || '%' OR LOWER(id::text) LIKE '%' || LOWER($1) || '%'", [search])
+    const {rows} = await pool.query("SELECT * FROM inventory WHERE LOWER(name) LIKE '%' || LOWER($1) || '%' OR id = $1", [search])
     return rows
 }
 
@@ -22,7 +22,7 @@ async function deleteItem(id){
 
 async function editItem(id, req){
     const {name, quantity, price} = req.body
-    await pool.query("UPDATE inventory SET name = $1, quantity = $2, price = $3", [name, price, quantity])
+    await pool.query("UPDATE inventory SET name = $1, quantity = $3, price = $2 WHERE id = $4", [name, price, quantity, id])
 }
 
 module.exports = {
