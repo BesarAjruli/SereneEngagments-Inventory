7598b11f0587e8c18e397b5810254bc83583f0ef
BesarAjruli <aquila1912@gmail.com>
2025-01-03 02:32:48 +0100

    Optimized pool

--- db/pool.js
+++ db/pool.js
@@ -4,6 +4,7 @@ const {Pool} = require('pg')
 
 module.exports = new Pool({
     connectionString: process.env.DATABASE_URL,
-    keepAlive: true,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
     ssl: {rejectUnauthorized: false}
 })
